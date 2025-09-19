ee6e3db4f1fe8bcee30e84785f280218d13cbf17
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-09-19 22:45:12 +0900

    update generate_json_schema_for_yaml.py

--- generate_json_schema_for_yaml.py
+++ generate_json_schema_for_yaml.py
@@ -12,12 +12,18 @@ def custom_tag_constructor(loader, node):
 # !vault などのYAMLタグのプロセッサを適当に定義する
 yaml.SafeLoader.add_constructor('!vault', custom_tag_constructor)
 
-# src_yml を読み込み dest_json に JSON Schema を出力する
-def convert(src_yml, dest_json, dump_json=False):
-    with open(src_yml, "r") as src_yml_file:
+def convert(src_yaml, dest_json, dump_json=False):
+    with open(src_yaml, "r") as src_yml_file:
         yaml_data = yaml.safe_load(src_yml_file)
     builder = SchemaBuilder()
     builder.add_object(yaml_data)
+    if not dest_json:
+        if dump_json:
+            import sys
+            json.dump(builder.to_schema(), sys.stdout, indent=2)
+        else:
+            print( yaml.dump(builder.to_schema(), default_flow_style=False) )
+        return
     with open(dest_json, "w") as file:
         if dump_json:
             json.dump(builder.to_schema(), file, indent=2)
@@ -26,7 +32,7 @@ def convert(src_yml, dest_json, dump_json=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="")
-    parser.add_argument("-i", "--input")
+    parser.add_argument("-i", "--input", required=True)
     parser.add_argument("-o", "--output")
     parser.add_argument("-j", "--json", action="store_true")
     args = parser.parse_args()
